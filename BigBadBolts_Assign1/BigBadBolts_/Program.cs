9c68a39c831700b1d5261f7c0a39dd9e6dfd375b
byronHogan1218 <byronhogan1218@gmail.com>
2019-09-06 13:33:28 -0500

    Added the post class barebones

--- BigBadBolts_Assign1/BigBadBolts_/Program.cs
+++ BigBadBolts_Assign1/BigBadBolts_/Program.cs
@@ -1,9 +1,58 @@
 ﻿using System;
-//I am byron
+using System.Collections.Generic;
+/**
+ * NOTES TO MY PARTNER: NONE FOR NOW 
+ */
 namespace BigBadBolts_
 {
     class Program
     {
+
+
+
+
+        public class Post
+        {
+            private readonly uint postId;
+            private string title;
+            private readonly uint authorID;
+            private string postContent;
+            private readonly uint subHome;
+            private uint upVotes;
+            private uint downVotes;
+            private uint weight;
+            private readonly DateTime timeStamp;
+            //private SortedSet<Comment> postComments; Uncomment this when the comments are written.
+
+            public string Title //This is my property for a post title
+            {
+                get { return title; }
+                set
+                {
+                    if (value.Length >= 1 && value.Length <= 100)
+                    {
+                        if (vulgarityChecker(value))//If true, found profanity
+                        {
+                            //TODO: IMPLEMENT THE FOUL LANGUAGE EXECPTION
+                        }
+                        else //did not find profanity
+                        {
+                            title = value;
+                        }
+                    }
+                }
+            }//end title property
+
+            private bool vulgarityChecker(string s)
+            {
+                return false;
+            }
+        }//End class
+
+        /**
+         * This is the main function of the program. It runs a loop that will quit out when the user enters 
+         * the correct option to quit. It mainly functions to call other functions to do the rest of the program.
+         */
         static void Main(string[] args)
         {
             bool exitProgram = false;
@@ -36,11 +85,11 @@ namespace BigBadBolts_
                     {
                         exitProgram = true;
                     }
-                    else if( userInput.ToLower() == "q" || userInput.ToLower() == "e")//handles the special single character exceptions
+                    else if (userInput.ToLower() == "q" || userInput.ToLower() == "e")//handles the special single character exceptions
                     {
                         exitProgram = true;
                     }
-                    else if(Char.IsDigit(userInput[0]))// Determines the function to call
+                    else if (Char.IsDigit(userInput[0]))// Determines the function to call
                     {
                         switch (userInput)
                         {
@@ -83,7 +132,7 @@ namespace BigBadBolts_
                 }
 
             }
-          
+
         }
     }
 }
